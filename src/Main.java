6d324542d13f5de738921fb18a701d633e52bf9d
Ivan <Ivanlesnyak55@gmail.com>
2022-10-17 10:03:59 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
     double d1,d2;
     float f = (float) 14.75;
     char c1='0';
+    //task2
+    l= (long) f;
+    d1 = Math.random() * 100;
+    d2 = ((Math.random() * (99 - 1)+1));
+    System.out.println("l="+l+"\n"+"d1="+d1+"\n"+"d2="+d2+"\n");
     }
 }
